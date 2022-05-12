fc4a132c6d578b419d9c13be4ce80a4cff04bedf
mango_gills <juzzgil21@gmail.com>
2022-05-12 16:36:22 +0800

    feat: added sub-menus
    sub-menu for feature and company links.
    display sub-menu on click.

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@ const burgerMenu = document.querySelector(".burger-menu");
 const linksContainer = document.querySelector(".links-container");
 
 const caretDirection = document.querySelectorAll(".caret");
+const features = document.querySelector(".features");
+const company = document.querySelector(".company");
 
 burgerMenu.addEventListener("click", () => {
   linksContainer.classList.toggle("hide");
@@ -11,4 +13,12 @@ caretDirection.forEach((caret) => {
   caret.addEventListener("click", () => {
     caret.lastChild.classList.toggle("active");
   });
+});
+
+features.addEventListener("click", () => {
+  document.getElementById("features-menu").classList.toggle("unhide");
+});
+
+company.addEventListener("click", () => {
+  document.getElementById("company-menu").classList.toggle("unhide");
 });
